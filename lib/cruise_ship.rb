b4cc2e438704bbe65eed7ed1e5e1140147c75353
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-21 04:41:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |key, value|
-    if key == :suite_a && value.starts_with?("a")
+    if key == :suite_a && value.starts_with?("A")
       winner = value
     end
   end
